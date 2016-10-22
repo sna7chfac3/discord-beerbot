7ccbe7b65289c9826f8392f8a65c046d3100c55b
niinzir <niinzir@gmail.com>
2016-10-21 22:07:20 -0700

    add beer searching function

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+client.login('MjM5MTE2OTcxMzkwNTMzNjMy.Cuw41g.ohzfXBPl9BSiDD6uhq-34xVEWQY')
+
+const BreweryDb = require('node-brewerydb');
+const brewdb = new BreweryDb({apiKey: "0a4fe898b6402baccb952720935f283a"});
 
 console.log('BeerBot has started')
+
+var result;
+
+function searchBeer(toSearch) {
+  brewdb.beers({name: toSearch}, function(err, res) {
+    if (err) {
+      console.log("An error has occured");
+    }
+    result = JSON.parse(res);
+    console.log(result)
+  });
+}
 
 client.on('message', message => {
   if (message.content === '!beerbot') {
@@ -9,4 +25,12 @@ client.on('message', message => {
   }
 });
 
-client.login('token')
+client.on('message', message => {
+  if (message.content.includes("!beerbot search")) {
+    var input = message.content
+    var nameToSearch = input.substr(input.lastIndexOf('search') + 7);
+    searchBeer(nameToSearch);
+    // console.log(result);
+    // message.reply(result);
+  }
+});
