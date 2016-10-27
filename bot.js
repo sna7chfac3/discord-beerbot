d3d9946fb1bf3dfd315e573ba0e14292416ab1d2
niinzir <niinzir@gmail.com>
2016-10-27 10:33:03 -0700

    rename "client" constant to "bot", rename "name" variable to "beerName"

--- bot.js
+++ bot.js
@@ -1,22 +1,25 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-client.login('discord-token');
+const bot = new Discord.Client();
+bot.login('discord-token');
 
 const BreweryDb = require('brewerydb-node');
 const brewdb = new BreweryDb('brewerydb-api-key');
 
-console.log('BeerBot has started');
+bot.on('ready', ready => {
+  console.log("BeerBot has started");
+})
 
-client.on('message', message => {
+// beer search
+bot.on('message', message => {
   if (message.content.includes("!beerbot search")) {
     var input = message.content;
-    var name = input.substr(input.indexOf('!') + 16)
-    brewdb.search.beers ({q: name}, function(err, data) {
+    var beerName = input.substr(input.indexOf('!') + 20)
+    brewdb.search.beers ({q: beerName}, function(err, data) {
       if (err) {
         message.reply("There was an error");
       } else if (data) {
         var result = data[0];
-        console.log(name);
+        console.log(beerName);
         message.channel.sendMessage("Name: " + result.name + "\nABV: " + result.abv + "\nType: " + result.style.shortName);
         if (typeof result.description !== 'undefined') {
           message.channel.sendMessage("Description: " + result.description);
@@ -29,7 +32,8 @@ client.on('message', message => {
   }
 });
 
-client.on('message', message => {
+// ping
+bot.on('message', message => {
   if (message.content === '!beerbot') {
     message.reply('pong');
   }
